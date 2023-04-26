c2e607d5c57debe89875adf917af13fd0e55665b
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-04-26 12:24:52 +0530

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Neeraj Gupta on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
